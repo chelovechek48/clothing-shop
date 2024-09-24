21481a11b4d5287fb7b93a656b7dc9f70cdc8848
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-09-24 06:12:38 +0300

    Добавил html разметку для hero блока

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,5 +1,39 @@
+<script setup lang="ts">
+</script>
+
 <template>
-	<section class="hero" />
+	<section class="hero">
+		<AppContainer class="hero__container">
+			<div class="hero__text">
+				<h1 class="hero__title">
+					Find clothes that matches your style
+				</h1>
+				<p class="hero__paragraph">
+					Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.
+				</p>
+				<NuxtLink
+					to="/"
+					class="hero__button"
+				>
+					Shop Now
+				</NuxtLink>
+				<ul class="hero__features">
+					<li class="hero__features-item">
+						<span class="hero__features-count">
+							200+
+						</span>
+						<span class="hero__features-label">
+							International Brands
+						</span>
+					</li>
+				</ul>
+			</div>
+			<img
+				src="" alt=""
+				class="hero__image"
+			>
+		</AppContainer>
+	</section>
 </template>
 
 <style lang="scss" scoped>
