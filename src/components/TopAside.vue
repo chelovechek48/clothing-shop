5bdf24890044dfc8893ac3d6887805b70fe5be47
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-09-24 06:03:10 +0300

    Добавил плашку со спец предложением при регистрации сверху страцицы

---
+++ src/components/TopAside.vue
@@ -0,0 +1,39 @@
+<template>
+	<aside class="aside">
+		<AppContainer>
+			Sign up and get 20% off to your first order.
+			<NuxtLink
+				to="/"
+				class="aside__link"
+			>
+				Sign Up Now
+			</NuxtLink>
+		</AppContainer>
+	</aside>
+</template>
+
+<style lang="scss" scoped>
+@use '@vars/colors';
+
+.aside {
+	font-size: 1rem;
+	text-align: center;
+
+	color: colors.$white;
+	background-color: colors.$black;
+	padding-block: 0.75rem;
+
+	&__link {
+		position: relative;
+		&::after {
+			content: '';
+			position: absolute;
+			bottom: -3px;
+			left: 0;
+			width: 100%;
+			height: 1px;
+			background-color: colors.$white;
+		}
+	}
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>
+		<TopAside />
 		<AppHeader />
 		<AppHero />
 	</div>
